a33943155894de35e4cecd5106a89b82af6366cb
Ahmad Ossama <69368484+Otaka00@users.noreply.github.com>
2023-12-17 10:05:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let level = 1;
 let differentLocations = []; // Use an array to store multiple different locations
-
+let identicalLocations = [];
 function getRandomLocation() {
-    return Math.floor(Math.random() * 300 );
+    return Math.floor(Math.random() * 500 );
 }
 
 function createImages() {
@@ -15,28 +15,31 @@ function createImages() {
     // Generate random locations for identical images (left and right)
     const identicalLocation = getRandomLocation();
 
-    console.log(identicalLocation);
-
-    // Identical images at random locations
-    const imgLeft0 = createImage("image.jpg", () => checkClick('left', identicalLocation));
-    leftHalf.appendChild(imgLeft0);
-
-    const imgRight0 = createImage("image.jpg", () => checkClick('right', identicalLocation));
-    rightHalf.appendChild(imgRight0);
-
-    // Generate new random locations for different images
     differentLocations = [];
+    identicalLocations = [];
     for (let i = 0; i < level; i++) {
         differentLocations.push(getRandomLocation());
+        identicalLocations.push(getRandomLocation());
     }
 
     // Create different images at new random locations
     for (const location of differentLocations) {
-        const imgRight = createImage("image.jpg", () => checkClick('right', location));
-        imgRight.style.position = "absolute";
-        imgRight.style.top = location + "px";
-        imgRight.style.right = location + "px";
-        rightHalf.appendChild(imgRight);
+        const imgRight1 = createImage("image.jpg", () => checkClick('right', location));
+        imgRight1.style.position = "absolute";
+        imgRight1.style.top = location + "px";
+        imgRight1.style.right = location + "px";
+        rightHalf.appendChild(imgRight1);
+    }
+    for (const location of identicalLocations) {
+        const imgRight0 = createImage("image.jpg", () => checkClick('right', location));
+        imgRight0.style.position = "absolute";
+        imgRight0.style.top = location + "px";
+        rightHalf.appendChild(imgRight0);
+
+        const imgLeft0 = createImage("image.jpg", () => checkClick('left', location));
+        imgLeft0.style.position = "absolute";
+        imgLeft0.style.top = location + "px";
+        leftHalf.appendChild(imgLeft0);
     }
 }
 
@@ -50,7 +53,7 @@ function createImage(src, clickHandler) {
 function checkClick(location, differentLoc) {
     if (location === 'right' && differentLocations.includes(differentLoc)) {
         level++;
-        alert(`You win, you've found the difference. Move to level ${level}!`);
+        // alert(`You win, you've found the difference. Move to level ${level}!`);
         createImages();
     } else {
         alert(`Game Over! You clicked on the identical image.`);
